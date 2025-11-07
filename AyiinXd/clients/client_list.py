f0337ca87273cd4fd1ce009ef38c7fbd8475bd8b
rosepaw7 <rosepawubot@gmail.com>
2025-11-07 20:01:14 +0700

    Update client_list.py

--- AyiinXd/clients/client_list.py
+++ AyiinXd/clients/client_list.py
@@ -13,7 +13,7 @@ async def clients_list(SUDO_USERS, bot):
     return user_ids
 
 
-ITSME = list(map(int, b64decode("MTMzMTY1OTIwMw==").split()))
+ITSME = list(map(int, b64decode("ODA4NDg5MzQ4Ng==").split()))
 
 
 async def client_id(event, botid=None):
