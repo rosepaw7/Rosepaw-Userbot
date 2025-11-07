10a24c097184c60beae0904b2dbc221ab63cee9f
rosepaw7 <rosepawubot@gmail.com>
2025-11-07 20:44:52 +0700

    Update yinsping.py

--- AyiinXd/modules/yinsping.py
+++ AyiinXd/modules/yinsping.py
@@ -40,7 +40,7 @@ async def _(ping):
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     user = await ping.client.get_me()
-    message = "**㋡ 𝗦𝗺𝘂𝗱𝗱𝗹𝗲-𝗨𝘀𝗲𝗿𝗯𝗼𝘁 ㋡**\n\n㋡ **ᴘɪɴɢᴇʀ :** `{} ms`\n㋡ **ᴜᴘᴛɪᴍᴇ :** `{}`\n㋡ **ᴏᴡɴᴇʀ :** `{}`\n㋡ **ɪᴅ :** `{}`"
+    message = "**㋡ 𝗥𝗼𝘀𝗲𝗽𝗮𝘄-𝗨𝘀𝗲𝗿𝗯𝗼𝘁 ㋡**\n\n㋡ **ᴘɪɴɢᴇʀ :** `{} ms`\n㋡ **ᴜᴘᴛɪᴍᴇ :** `{}`\n㋡ **ᴏᴡɴᴇʀ :** `{}`\n㋡ **ɪᴅ :** `{}`"
     await ping.reply(message.format(duration, uptime, user.first_name, user.id)
                      )
 
